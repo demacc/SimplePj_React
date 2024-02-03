52396c1b8e610bf63ea1b41bdb41a95b4e17eb76
Thein Htike Aung <senku9123@gmail.com>
2024-02-03 20:21:48 +0630

    simpleTimer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Hello world</h2>
-      </div>
-    );
-  }
-}
+export default function App() {
+  let [timer, setTimer] = useState(0);
 
-export default App;
+  let startTimer = () => {
+    window.myTimer = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+  let stopTimer = () => {
+    clearInterval(window.myTimer);
+  };
+  let resetTimer = () => {
+    clearInterval(window.myTimer);
+    setTimer(0);
+  };
+  return (
+    <div className="container">
+      <h2>Timer</h2>
+      <div className="time-container">
+        <span> {Math.trunc(timer / 60)}minus</span>
+        <span>{timer % 60}secs</span>
+      </div>
+      <div className="btn-container">
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
